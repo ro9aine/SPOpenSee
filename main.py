7ab570030270b0573efd64f3b3675461e627a3e4
unknown <ro9aine@gmail.com>
2026-03-01 23:22:50 +0300

    init

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-from src.OpenSeeFace.facetracker import ...
+import cv2
+from OpenSeeFace.tracker import Tracker
+
+tracker = Tracker(640, 640)
+results = tracker.predict(cv2.imread('./test.jpg'))
+for f in results:
+    print(f.success)
+    print(f.eye_blink[0], f.eye_blink[1])
+    print(f.lms)
